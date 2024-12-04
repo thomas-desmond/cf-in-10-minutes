d01dd50b5ca53934a64e9bf179804d50bb8f398d
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2024-12-04 11:40:36 -0800

    new cachedCompliment route

---
+++ app/api/cachedCompliment/route.ts
@@ -0,0 +1,33 @@
+import type { NextRequest } from "next/server";
+import { getRequestContext } from "@cloudflare/next-on-pages";
+
+export const runtime = "edge";
+
+interface UserRequest {
+  name: string;
+}
+
+export async function POST(request: NextRequest) {
+  const body: UserRequest = await request.json();
+
+  const cachedResponse = await getRequestContext().env.kv_demo33.get(body.name)
+  if(cachedResponse) return Response.json(cachedResponse)
+
+  const messages = [
+    {
+      role: "system",
+      content:
+        "You are a friendly and supportive AI bot designed to give personalized, thoughtful compliments to conference attendees. When someone provides their name, respond with a unique compliment that resonates with their skills, enthusiasm, and presence at a tech event. Compliments should be creative, authentic, and tailored to a tech-savvy audience, making them feel recognized and appreciated for their contributions to the industry. Provide your response in a succinct sentence or two.",
+    },
+    {
+      role: "user",
+      content: body.name,
+    },
+  ];
+
+  const {response} = await getRequestContext().env.AI.run("@cf/meta/llama-3.1-8b-instruct-fast", { messages });
+
+  await getRequestContext().env.kv_demo.put(body.name, response)
+
+  return Response.json(response);
+}
